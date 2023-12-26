dcac16c99982f6c01761d774e153309732c6bbf6
Ravid Pollak <>
2023-12-26 13:48:49 +0200

    added event listener for post message from iframe

--- src/components/ui/AppchargeCheckout/index.tsx
+++ src/components/ui/AppchargeCheckout/index.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import "./button.scss";
 import { Stack } from "@mui/material";
 
@@ -14,7 +14,24 @@ export interface AppchargeCheckoutProps {
   onCloseModal?: () => void;
   isOpen?: boolean;
   bootLocation?: string;
+  // new props - should delete old one
+  domain: string;
+  onOpen?: () => void;
+  onClose?: () => void;
+  onPaymentIntentSuccess?: () => void;
+  onPaymentIntentFailed?: () => void;
+  onOrderCompletedSucessfuly?: () => void;
+  onOrderCompletedFailed?: () => void;
 }
+
+enum EventEnum {
+  paymentIntentSuccess = "Payment_intent_success",
+  paymentIntentFailed = "Payment_intent_failed",
+  orderCompletedSucessfuly = "Order_completed_sucessfuly",
+  orderCompletedFailed = "Order_completed_failed",
+}
+
+// export interface AppchargeCheckoutPropsV2 {}
 
 function AppchargeCheckout({
   playerId,
@@ -23,23 +40,60 @@ function AppchargeCheckout({
   width = "450px",
   height = "500px",
   url,
-  closeIconColor,
   closeIconRender,
   onCloseModal,
-  isOpen,
   bootLocation,
+  isOpen,
+  domain,
+  onPaymentIntentSuccess,
+  onPaymentIntentFailed,
+  onOrderCompletedSucessfuly,
+  onOrderCompletedFailed,
 }: AppchargeCheckoutProps) {
+  
+  useEffect(() => {
+    const eventHandler = (event: MessageEvent) => {
+      if (event.origin === domain) return;
+
+      switch (event.data) {
+        case EventEnum.orderCompletedFailed:
+          onOrderCompletedFailed?.();
+          break;
+        case EventEnum.orderCompletedSucessfuly:
+          onOrderCompletedSucessfuly?.();
+          break;
+        case EventEnum.paymentIntentFailed:
+          onPaymentIntentFailed?.();
+          break;
+        case EventEnum.paymentIntentSuccess:
+          onPaymentIntentSuccess?.();
+          break;
+      }
+    };
+
+    window.addEventListener("message", eventHandler);
+
+    return () => {
+      window.removeEventListener("message", eventHandler);
+    };
+  }, [
+    onPaymentIntentSuccess,
+    onPaymentIntentFailed,
+    onOrderCompletedSucessfuly,
+    onOrderCompletedFailed,
+  ]);
+
   return (
     <Stack
-      position='absolute'
-      width='100vw'
-      height='100vh'
-      bgcolor='rgba(0, 0, 0, 0.5)'
+      position="absolute"
+      width="100vw"
+      height="100vh"
+      bgcolor="rgba(0, 0, 0, 0.5)"
       top={0}
       left={0}
-      justifyContent='center'
-      alignItems='center'
-      zIndex='99999'
+      justifyContent="center"
+      alignItems="center"
+      zIndex="99999"
       style={{
         zIndex: "999999",
       }}
@@ -54,22 +108,22 @@ function AppchargeCheckout({
           width: width,
           height: height,
         }}
-        position='relative'
-        justifyContent='center'
-        alignItems='center'
+        position="relative"
+        justifyContent="center"
+        alignItems="center"
       >
         <button style={{ background: "transparent", border: "0" }}>
           {closeIconRender ? (
             closeIconRender
           ) : (
             <div>
-              <svg className='svg-icon' viewBox='0 0 20 20'>
+              <svg className="svg-icon" viewBox="0 0 20 20">
                 <path
-                  fill='none'
-                  d='M11.469,10l7.08-7.08c0.406-0.406,0.406-1.064,0-1.469c-0.406-0.406-1.063-0.406-1.469,0L10,8.53l-7.081-7.08
+                  fill="none"
+                  d="M11.469,10l7.08-7.08c0.406-0.406,0.406-1.064,0-1.469c-0.406-0.406-1.063-0.406-1.469,0L10,8.53l-7.081-7.08
 							c-0.406-0.406-1.064-0.406-1.469,0c-0.406,0.406-0.406,1.063,0,1.469L8.531,10L1.45,17.081c-0.406,0.406-0.406,1.064,0,1.469
 							c0.203,0.203,0.469,0.304,0.735,0.304c0.266,0,0.531-0.101,0.735-0.304L10,11.469l7.08,7.081c0.203,0.203,0.469,0.304,0.735,0.304
-							c0.267,0,0.532-0.101,0.735-0.304c0.406-0.406,0.406-1.064,0-1.469L11.469,10z'
+							c0.267,0,0.532-0.101,0.735-0.304c0.406-0.406,0.406-1.064,0-1.469L11.469,10z"
                 ></path>
               </svg>
             </div>
@@ -81,7 +135,7 @@ function AppchargeCheckout({
           }&bootLocation=${
             bootLocation || window.location.origin.replace("https://", "")
           }`}
-          title='checkout'
+          title="checkout"
           style={{
             position: "relative",
             zIndex: 300,
