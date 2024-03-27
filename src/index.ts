2597acac643af7ac657a4dd451891b2a7ebcefe3
kovlerAppcharge <ariel@appcharge.com>
2024-03-27 17:44:51 +0200

    added get price points util

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as AppchargeCheckout } from './components/ui/AppchargeCheckout';
 export { default as AppchargeCheckoutInit } from './components/ui/AppchargeCheckoutInit';
+export { getPricePoints } from './utils/price-points-util';
